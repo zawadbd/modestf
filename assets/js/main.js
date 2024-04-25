3211e3c539f8c24e7055f66d01876ef415f8bef8
Md Zahirul Islam <143722636+zawadbd@users.noreply.github.com>
2024-04-26 00:21:56 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    startPosition:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    animateIn:true,
+    animateOut:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
